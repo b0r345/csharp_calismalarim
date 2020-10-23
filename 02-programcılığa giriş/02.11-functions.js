df1302c90e6520557e065e3a8b50398c4b5a790f
b0r345 <boreas17-83@hotmail.com>
2020-10-23 22:53:21 +0300

    update

--- 02-programcılığa giriş/02.11-functions.js
+++ 02-programcılığa giriş/02.11-functions.js
@@ -1,8 +1,8 @@
-var sayi1=6
-var sayi2=16
-var sayi3=3
-var sayi4=48
-var sayi5=49
-var sayi6=10
+var sayi1=Math.ceil(Math.random()*49)
+var sayi2=Math.ceil(Math.random()*49)
+var sayi3=Math.ceil(Math.random()*49)
+var sayi4=Math.ceil(Math.random()*49)
+var sayi5=Math.ceil(Math.random()*49)
+var sayi6=Math.ceil(Math.random()*49)
 
 console.log("Kolon: "+sayi1+" "+sayi2+" "+sayi3+" "+sayi4+" "+sayi5+" "+sayi6)
