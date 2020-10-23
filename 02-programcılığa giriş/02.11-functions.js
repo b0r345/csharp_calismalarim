c4925f742e10eb70ef64c30b020687b8ec61816f
b0r345 <boreas17-83@hotmail.com>
2020-10-23 23:23:38 +0300

    update

--- 02-programcılığa giriş/02.11-functions.js
+++ 02-programcılığa giriş/02.11-functions.js
@@ -1,13 +1,16 @@
-function sayiUret(){
-    return Math.ceil(Math.random() *49)
+var deger="Deger"//global bir değişken olduğu için her yerden erişilebilir
+function sayiUret(ustLimit=49){//default değer verilirse parametre verilmediği zaman varsayılan değer tercih edilir
+    console.log(deger)
+    return Math.ceil(Math.random() *ustLimit)
 }
-
+console.log(deger)
+//console.log(ustLimit)//fonksiyon içeriğinde tanımlanan değişken global olmadığı için ana programdan erişilemez
 var sayi1=sayiUret()
-var sayi2=sayiUret()
+var sayi2=sayiUret(60)
 var sayi3=sayiUret()
-var sayi4=sayiUret()
-var sayi5=sayiUret()
-var sayi6=sayiUret()
+var sayi4=sayiUret(30)
+var sayi5=sayiUret(49)
+var sayi6=sayiUret(60)
 //her yerde değişiklik yapmak istediğimiz için fonksiyonlar bu konuda bize yardımcı oluyor
 
 
