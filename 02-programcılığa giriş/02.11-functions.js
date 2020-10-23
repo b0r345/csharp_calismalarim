0cee6a75e5044b59712526f38fe09c28058a0e92
b0r345 <boreas17-83@hotmail.com>
2020-10-23 23:08:31 +0300

    update

--- 02-programcılığa giriş/02.11-functions.js
+++ 02-programcılığa giriş/02.11-functions.js
@@ -1,8 +1,15 @@
+function sayiUret(){
+    return Math.ceil(Math.random() *49)
+}
+
+sayiUret()
 var sayi1=Math.ceil(Math.random()*49)
 var sayi2=Math.ceil(Math.random()*49)
 var sayi3=Math.ceil(Math.random()*49)
 var sayi4=Math.ceil(Math.random()*49)
 var sayi5=Math.ceil(Math.random()*49)
 var sayi6=Math.ceil(Math.random()*49)
+//her yerde değişiklik yapmak istediğimiz için fonksiyonlar bu konuda bize yardımcı oluyor
+
 
 console.log("Kolon: "+sayi1+" "+sayi2+" "+sayi3+" "+sayi4+" "+sayi5+" "+sayi6)
